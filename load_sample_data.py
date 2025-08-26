861b8ac164f72b39aa63b2602572a1e65b5829d0
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-26 17:03:36 +0700

    Update load_sample_data.py

--- load_sample_data.py
+++ load_sample_data.py
@@ -14,12 +14,12 @@ def load_sample_data():
     df['Year'] = df['Year'].astype(int).astype(str)
     df['Month'] = df['Month'].astype(int).astype(str).str.zfill(2)
     time.sleep(0.3)
-    """
 
     # Step 3: Create Period column
     df['Period'] = pd.to_datetime(df['Year'] + "-" + df['Month'], format="%Y-%m")
     time.sleep(0.3)
-
+    """
+    
     return df
 
 def init_session_state():
