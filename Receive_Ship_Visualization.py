a61b79c9263c81595b7d488844e177a6ee6e2d46
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-26 14:53:30 +0700

    Update Receive_Ship_Visualization.py

--- Receive_Ship_Visualization.py
+++ Receive_Ship_Visualization.py
@@ -2,7 +2,7 @@ import streamlit as st
 from load_sample_data import init_session_state
 from receive_ship_chart_page import render_chart_page
 
-st.set_page_config(page_title="Monthly Profit and Loss (User Interface)", layout="wide")
+st.set_page_config(page_title="Receive-Ship Visualization", layout="wide")
 
 # --- Initialize session state with cached data ---
 init_session_state()
