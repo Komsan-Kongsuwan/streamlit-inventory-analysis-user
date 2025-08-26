0f72212b562c72581334751a9c79728abed895b1
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-26 16:24:31 +0700

    Update receive_ship_chart_page.py

--- receive_ship_chart_page.py
+++ receive_ship_chart_page.py
@@ -59,8 +59,9 @@ def render_chart_page():
     if selected_year != "ALL":
         df_filtered = df_filtered[df_filtered["Year"] == selected_year]
     if selected_month_num:
+        st.write(selected_month_num)
+        st.dataframe(df_filtered["Month"])
         df_filtered = df_filtered[df_filtered["Month"] == selected_month_num]
-        st.dataframe(df_filtered)
     if items:
         df_filtered = df_filtered[df_filtered["Item Code"].isin(items)]
     if df_filtered.empty:
