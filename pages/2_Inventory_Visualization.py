22cb01f9435b8952b496d2512cd0e9e0464e18e5
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-27 09:06:31 +0700

    Update 2_Inventory_Visualization.py

--- pages/2_Inventory_Visualization.py
+++ pages/2_Inventory_Visualization.py
@@ -2,7 +2,7 @@ import streamlit as st
 from load_inventory_sample_data import init_session_state
 from inventory_chart_page import render_chart_page
 
-st.set_page_config(page_title="Receive-Ship Visualization", layout="wide")
+st.set_page_config(page_title="Inventory Visualization", layout="wide")
 
 # --- Initialize session state with cached data ---
 init_session_state()
