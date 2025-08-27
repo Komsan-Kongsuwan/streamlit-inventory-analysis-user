18b47990e50e3571e6642a73eb4ff7697392f6e6
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-27 21:43:15 +0700

    Update inventory_chart_page.py

--- inventory_chart_page.py
+++ inventory_chart_page.py
@@ -61,8 +61,6 @@ def render_chart_page():
         df_filtered = df_filtered[df_filtered["Year"] == selected_year]
     if selected_month_num != "ALL":
         df_filtered = df_filtered[df_filtered["Month"] == selected_month_num]
-    else:
-        df_filtered = df_filtered[df_filtered["Year"] == selected_year]
     if items:
         df_filtered = df_filtered[df_filtered["Item Code"].isin(items)]
     if df_filtered.empty:
