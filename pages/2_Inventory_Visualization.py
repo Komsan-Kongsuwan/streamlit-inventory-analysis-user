9dd7617884b8cf419e3a2c2d1b9a38a89241ac65
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-27 08:51:48 +0700

    Update 2_Inventory_Visualization.py

--- pages/2_Inventory_Visualization.py
+++ pages/2_Inventory_Visualization.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from load_sample_data import init_session_state
-from receive_ship_chart_page import render_chart_page
+from load_inventory_sample_data import init_session_state
+from inventory_chart_page import render_chart_page
 
 st.set_page_config(page_title="Receive-Ship Visualization", layout="wide")
 
