fe68b8a7ffebc5157d4b13538779f42d63e2fcce
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-27 21:58:24 +0700

    Update inventory_chart_page.py

--- inventory_chart_page.py
+++ inventory_chart_page.py
@@ -84,7 +84,7 @@ def render_chart_page():
         chart_df = chart_df.set_index(["Month","Rcv So Flag"]).reindex(all_months_flags, fill_value=0).reset_index()
         chart_df["x_label"] = chart_df["Month"].apply(lambda m: calendar.month_abbr[m])
         chart_title = f"📊 Monthly Stock in {selected_year}"
-    elif selected_year == "ALL"
+    elif selected_year == "ALL":
         chart_df = df_filtered.groupby(["Operation Date","Rcv So Flag"], as_index=False)["Quantity[Unit1]"].sum()
         all_months_flags = pd.MultiIndex.from_product([months, chart_df["Rcv So Flag"].unique()], names=["Month","Rcv So Flag"])
         chart_df = chart_df.set_index(["Operation Date","Rcv So Flag"]).reindex(all_months_flags, fill_value=0).reset_index()
