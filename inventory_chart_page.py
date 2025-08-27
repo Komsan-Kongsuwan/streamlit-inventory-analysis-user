08a434d1c61931302a95c093eb63299633cd22fe
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-27 21:22:57 +0700

    Update inventory_chart_page.py

--- inventory_chart_page.py
+++ inventory_chart_page.py
@@ -71,6 +71,7 @@ def render_chart_page():
     # 📊 CHART
     # ==========================================================
     if selected_month_num:
+        st.write(selected_month_num)
         df_filtered["Day"] = pd.to_datetime(df_filtered["Operation Date"]).dt.day
         total_days = pd.Series(range(1,32))
         chart_df = df_filtered.groupby(["Day","Rcv So Flag"], as_index=False)["Quantity[Unit1]"].sum()
